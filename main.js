1867e24338052062173b8263c24bbd42679a5aea
boldendc <70532342+boldendc@users.noreply.github.com>
2020-10-29 20:40:33 +0000

    Lab 2 part 1 of 2

--- main.js
+++ main.js
@@ -125,11 +125,14 @@ function main() {
     console.log(`\n--- Test Number ${i + 1} getIpv4MappedIpv6Address(${sampleIpv4s[i]}) ---`);
     // Assign the function results to a variable so we can check if a string or null was returned.
     let mappedAddress = getIpv4MappedIpv6Address(sampleIpv4s[i]);
+
+  getIpv4MappedIpv6Address(mappedAddress => {
     if( mappedAddress ) {
       console.log(`  IPv4 ${sampleIpv4s[i]} mapped to IPv6 Address: ${mappedAddress}`);
     } else {
       console.error(`  Problem converting IPv4 ${sampleIpv4s[i]} into a mapped IPv6 address.`);
     }
+    });
   }
 }
 
